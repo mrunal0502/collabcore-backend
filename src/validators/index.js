61f3a78dc594cd8c6b6a868be8ec63785a7720d1
Mrunal <mrunalrawade05@gmail.com>
2026-02-24 20:12:55 +0530

    Write validators for login and register to check fot user's input values

---
+++ src/validators/index.js
@@ -0,0 +1,41 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email format"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be in lowercase")
+      .isLength({ min: 3, max: 30 })
+      .withMessage("Username must be between 3 and 30 characters"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 3, max: 30 })
+      .withMessage("Username must be between 3 and 30 characters"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+const userLoginValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email format"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+  ];
+};
+
+export { userRegisterValidator, userLoginValidator };
